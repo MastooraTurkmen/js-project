3f32953d54287dea2d77c036fbe769ff4dc7607e
MastooraTurkmen <132576850+MastooraTurkmen@users.noreply.github.com>
2023-10-24 10:37:27 +0430

    my commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import { playlistArr } from '/playlist.js'
+
+const playlistHtml = playlistArr.map(function(track){
+    return `
+    <section class="card">
+        <div class="card-start">
+            <img src="/images/${track.albumArt}">
+        </div>
+            <div class="card-mid">
+                <h4 class="card-title">${track.title}</h4>
+                <p class="card-artist">${track.artist}</p>
+            </div>
+        <div class="card-end">
+            <p class="card-menu">...</p>
+        </div>
+    </section>
+    `
+}).join('')
+
+document.getElementById('container').innerHTML = playlistHtml
---
+++ playlist.js
@@ -0,0 +1,22 @@
+export const playlistArr = [
+    {
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        albumArt: 'bohemian-rhapsody.png'
+    },
+    {
+        title: 'As It Was',
+        artist: 'Harry Styles',
+        albumArt: 'as-it-was.png'
+    },
+    {
+        title: 'Stairway to Heaven',
+        artist: 'Led Zeppelin',
+        albumArt: 'stairway-to-heaven.png'
+    },
+    {
+        title: 'Therefore I Am',
+        artist: 'Billie Eilish',
+        albumArt: 'therefore-i-am.png'
+    }
+]
