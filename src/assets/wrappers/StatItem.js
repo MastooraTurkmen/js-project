f3bd58ff76fd453b9afe243bfb95d917d0a6a26b
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-02 21:12:38 +0430

    set up

--- src/assets/wrappers/StatItem.js
+++ src/assets/wrappers/StatItem.js
@@ -16,7 +16,26 @@ const Wrapper = styled.article`
     font-size: 50px;
     color: ${(props) => props.color};
   }
-  
+  .title {
+    margin: 0;
+    text-transform: capitalize;
+    letter-spacing: var(--letterSpacing);
+    text-align: left;
+    margin-top: 0.5rem;
+  }
+  .icon {
+    width: 70px;
+    height: 60px;
+    background: ${(props) => props.bcg};
+    border-radius: var(--borderRadius);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      font-size: 2rem;
+      color: ${(props) => props.color};
+    }
+  }
 `
 
 export default Wrapper
