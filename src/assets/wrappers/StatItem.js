04d6ae3b8de680d54b14911ef0082f003ff1183c
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-02 21:12:17 +0430

    set up

--- src/assets/wrappers/StatItem.js
+++ src/assets/wrappers/StatItem.js
@@ -1,1 +1,16 @@
 import styled from 'styled-components'
+
+const Wrapper = styled.article`
+  padding: 2rem;
+  background: var(--white);
+  border-radius: var(--borderRadius);
+  border-bottom: 5px solid ${(props) => props.color};
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+`
+
+export default Wrapper
