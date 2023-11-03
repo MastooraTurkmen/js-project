f2e768c2d60ba563f32535f925c87cd77090ab0b
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:33:57 +0430

    set up

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -11,6 +11,24 @@ const SmallSidebar = () => {
   const toggle = () => {
     dispatch(toggleSidebar());
   };
-
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button className='close-btn' onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggle} />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default SmallSidebar;
