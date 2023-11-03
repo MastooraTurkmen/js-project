d5e6d35ac82b90d716330b6a16fd89874b250d73
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:33:40 +0430

    set up

---
+++ src/components/SmallSidebar.js
@@ -0,0 +1,10 @@
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
+import NavLinks from './NavLinks';
+const SmallSidebar = () => {
+
+};
+export default SmallSidebar;
