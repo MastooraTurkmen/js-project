9c2d2e22f37ebf4efb91cd043be49c9de70cc294
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:32:58 +0430

    set up

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -36,6 +36,52 @@ const SearchContainer = () => {
     dispatch(clearFilters());
   };
 
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h4>search form</h4>
+        <div className='form-center'>
+          {/* search position */}
+          <FormRow
+            type='text'
+            name='search'
+            value={localSearch}
+            handleChange={optimizedDebounce}
+          />
+          {/* search by status */}
+          <FormRowSelect
+            labelText='status'
+            name='searchStatus'
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          />
 
+          {/* search by type*/}
+          <FormRowSelect
+            labelText='type'
+            name='searchType'
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          />
+          {/* sort */}
+          <FormRowSelect
+            name='sort'
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button
+            className='btn btn-block btn-danger'
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 export default SearchContainer;
