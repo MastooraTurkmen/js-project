4ad3538ac21a5d3234e5a916ef13f39b2c4f0a37
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:24:35 +0430

    set up

---
+++ src/components/BarChart.js
@@ -0,0 +1,14 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+
+const BarChartComponent = ({ data }) => {
+
+};
+export default BarChartComponent;
