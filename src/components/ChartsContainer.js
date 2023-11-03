a139c06b1c98c4cd7fce52cb0bca3cf368344ed4
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:25:22 +0430

    set up

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -7,6 +7,14 @@ import { useSelector } from 'react-redux';
 const ChartsContainer = () => {
   const [barChart, setBarChart] = useState(true);
   const { monthlyApplications: data } = useSelector((store) => store.allJobs);
-  
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 export default ChartsContainer;
