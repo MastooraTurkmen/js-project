2b1ec73230c51d1a479aee644cae58ad0cf1b37d
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:25:10 +0430

    set up

---
+++ src/components/ChartsContainer.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+import { useSelector } from 'react-redux';
+const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useSelector((store) => store.allJobs);
+  
+};
+export default ChartsContainer;
