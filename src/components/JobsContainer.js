d53002c8e0ddb700dcf015ba3402fe3491471183
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:28:01 +0430

    set up

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -36,7 +36,17 @@ const JobsContainer = () => {
   }
 
   return (
-
+    <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && 's'} found
+      </h5>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+      {numOfPages > 1 && <PageBtnContainer />}
+    </Wrapper>
   );
 };
 export default JobsContainer;
