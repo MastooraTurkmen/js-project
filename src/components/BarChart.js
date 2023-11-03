561be92c8e56e8efbcea7b5b76fa081336696dd2
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:24:43 +0430

    set up

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -9,6 +9,16 @@ import {
 } from 'recharts';
 
 const BarChartComponent = ({ data }) => {
-
+  return (
+    <ResponsiveContainer width='100%' height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='10 10 ' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey='count' fill='#3b82f6' barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
 export default BarChartComponent;
