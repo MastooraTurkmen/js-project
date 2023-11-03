1db3ca69e550a33e505300bf94f943c860018803
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-03 21:24:03 +0430

    set up

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -6,6 +6,23 @@ import { useSelector } from 'react-redux';
 const BigSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
 
-  
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen
+            ? 'sidebar-container '
+            : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default BigSidebar;
