16eacffcc290ae0a3c73d613b9970ac4f7243c77
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-05 21:45:11 +0430

    set up

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -3,5 +3,11 @@ import { StatsContainer, Loading, ChartsContainer } from '../../components';
 import { useDispatch, useSelector } from 'react-redux';
 import { showStats } from '../../features/allJobs/allJobsSlice';
 
+const Stats = () => {
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
 
+
+};
 export default Stats;
