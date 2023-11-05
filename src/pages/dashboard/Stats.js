3b71a374a1105d86468d53f716f757e0f50ddaa1
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-05 21:45:00 +0430

    set up

---
+++ src/pages/dashboard/Stats.js
@@ -0,0 +1,7 @@
+import { useEffect } from 'react';
+import { StatsContainer, Loading, ChartsContainer } from '../../components';
+import { useDispatch, useSelector } from 'react-redux';
+import { showStats } from '../../features/allJobs/allJobsSlice';
+
+
+export default Stats;
