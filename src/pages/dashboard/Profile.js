941df604f9b18535a98be780e53350e361706775
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-05 21:48:52 +0430

    set up

---
+++ src/pages/dashboard/Profile.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { FormRow } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { updateUser } from '../../features/user/userSlice';
+
+const Profile = () => {
+  const { isLoading, user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    name: user?.name || '',
+    email: user?.email || '',
+    lastName: user?.lastName || '',
+    location: user?.location || '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, lastName, location } = userData;
+    if (!name || !email || !lastName || !location) {
+      toast.error('please fill out all fields');
+      return;
+    }
+    dispatch(updateUser(userData));
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+};
+export default Profile;
