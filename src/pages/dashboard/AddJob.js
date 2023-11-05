f5d5f83893920885406a1ac01eac01f6ab1acdeb
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-05 21:45:51 +0430

    set up

---
+++ src/pages/dashboard/AddJob.js
@@ -0,0 +1,64 @@
+import { FormRow, FormRowSelect } from '../../components';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import {
+  handleChange,
+  clearValues,
+  createJob,
+  editJob,
+} from '../../features/job/jobSlice';
+import { useEffect } from 'react';
+const AddJob = () => {
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId,
+  } = useSelector((store) => store.job);
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      toast.error('Please fill out all fields');
+      return;
+    }
+    if (isEditing) {
+      dispatch(
+        editJob({
+          jobId: editJobId,
+          job: { position, company, jobLocation, jobType, status },
+        })
+      );
+      return;
+    }
+    dispatch(createJob({ position, company, jobLocation, jobType, status }));
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    dispatch(handleChange({ name, value }));
+  };
+
+  useEffect(() => {
+    if (!isEditing) {
+      dispatch(
+        handleChange({
+          name: 'jobLocation',
+          value: user.location,
+        })
+      );
+    }
+  }, []);
+
+};
+export default AddJob;
