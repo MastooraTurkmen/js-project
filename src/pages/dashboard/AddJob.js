5b7ab8cb9b29de5213d268897c49fe7c887d9694
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-05 21:45:57 +0430

    set up

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -60,5 +60,68 @@ const AddJob = () => {
     }
   }, []);
 
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+        <div className='form-center'>
+          {/* position */}
+          <FormRow
+            type='text'
+            name='position'
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            type='text'
+            name='company'
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* jobLocation */}
+          <FormRow
+            type='text'
+            name='jobLocation'
+            labelText='job location'
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          {/* status */}
+          <FormRowSelect
+            name='status'
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          />
+          {/* job type*/}
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
+          />
+          <div className='btn-container'>
+            <button
+              type='button'
+              className='btn btn-block clear-btn'
+              onClick={() => dispatch(clearValues())}
+            >
+              clear
+            </button>
+            <button
+              type='submit'
+              className='btn btn-block submit-btn'
+              onClick={handleSubmit}
+              disabled={isLoading}
+            >
+              submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 export default AddJob;
