7524625b31124c6532ca9f01113bc80ba7490d3e
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-05 21:43:24 +0430

    set up

---
+++ src/pages/Landing.js
@@ -0,0 +1,9 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+
+};
+
+export default Landing;
