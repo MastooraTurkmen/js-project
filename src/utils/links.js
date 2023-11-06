02a373415d8384ad7bf400b81a1fcbc902038911
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 22:01:37 +0430

    set up

--- src/utils/links.js
+++ src/utils/links.js
@@ -3,3 +3,10 @@ import { MdQueryStats } from 'react-icons/md';
 import { FaWpforms } from 'react-icons/fa';
 import { ImProfile } from 'react-icons/im';
 
+const links = [
+  { id: 1, text: 'stats', path: '/', icon: <IoBarChartSharp /> },
+  { id: 2, text: 'all jobs', path: 'all-jobs', icon: <MdQueryStats /> },
+  { id: 3, text: 'add job', path: 'add-job', icon: <FaWpforms /> },
+  { id: 4, text: 'profile', path: 'profile', icon: <ImProfile /> },
+];
+
