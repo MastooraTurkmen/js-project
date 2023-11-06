9fda653a39bca89f57b9e08de6c85f55cdc884ba
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 22:00:39 +0430

    set up

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,3 +2,10 @@ import axios from 'axios';
 import { clearStore } from '../features/user/userSlice';
 import { getUserFromLocalStorage } from './localStorage';
 
+const customFetch = axios.create({
+  baseURL: 'https://redux-toolkit-jobster-api-server.onrender.com/api/v1',
+});
+
+
+
+export default customFetch;
