0a4cf53efaade782ca4c64b601fb76dc73c46358
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:57:52 +0430

    set up

---
+++ src/features/user/userThunk.js
@@ -0,0 +1,14 @@
+import customFetch, { checkForUnauthorizedResponse } from '../../utils/axios';
+import { clearAllJobsState } from '../allJobs/allJobsSlice';
+import { clearValues } from '../job/jobSlice';
+import { logoutUser } from './userSlice';
+export const registerUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.post(url, user);
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
+
+};
