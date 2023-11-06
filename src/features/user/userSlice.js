af2f31c72fe00258dee73938954dc729d9832d92
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:55:59 +0430

    set up

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,7 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import {
+  addUserToLocalStorage,
+  getUserFromLocalStorage,
+  removeUserFromLocalStorage,
+} from '../../utils/localStorage';
