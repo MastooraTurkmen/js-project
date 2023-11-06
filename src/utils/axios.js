ca865beac5757c42c5250d8dd9f6b2a87e94f20a
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 22:00:47 +0430

    set up

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -6,6 +6,12 @@ const customFetch = axios.create({
   baseURL: 'https://redux-toolkit-jobster-api-server.onrender.com/api/v1',
 });
 
-
+customFetch.interceptors.request.use((config) => {
+  const user = getUserFromLocalStorage();
+  if (user) {
+    config.headers['Authorization'] = `Bearer ${user.token}`;
+  }
+  return config;
+});
 
 export default customFetch;
