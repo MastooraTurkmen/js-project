be0268564368d1c73bf1a54358812fe8d738abe2
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:54:18 +0430

    set up

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -38,7 +38,37 @@ const jobSlice = createSlice({
       return { ...state, isEditing: true, ...payload };
     },
   },
-
+  extraReducers: (builder) => {
+    builder
+      .addCase(createJob.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createJob.fulfilled, (state) => {
+        state.isLoading = false;
+        toast.success('Job Created');
+      })
+      .addCase(createJob.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      })
+      .addCase(deleteJob.fulfilled, (state, { payload }) => {
+        toast.success(payload);
+      })
+      .addCase(deleteJob.rejected, (state, { payload }) => {
+        toast.error(payload);
+      })
+      .addCase(editJob.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(editJob.fulfilled, (state) => {
+        state.isLoading = false;
+        toast.success('Job Modified...');
+      })
+      .addCase(editJob.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export const { handleChange, clearValues, setEditJob } = jobSlice.actions;
