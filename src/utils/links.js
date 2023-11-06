a906ebe1025eeb27e8e69565785124c3486754c2
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 22:01:29 +0430

    set up

---
+++ src/utils/links.js
@@ -0,0 +1,5 @@
+import { IoBarChartSharp } from 'react-icons/io5';
+import { MdQueryStats } from 'react-icons/md';
+import { FaWpforms } from 'react-icons/fa';
+import { ImProfile } from 'react-icons/im';
+
