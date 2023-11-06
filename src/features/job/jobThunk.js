7d651421f5c85a465c01ee5c86165d525a15cb0e
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:54:45 +0430

    set up

---
+++ src/features/job/jobThunk.js
@@ -0,0 +1,7 @@
+import { showLoading, hideLoading, getAllJobs } from '../allJobs/allJobsSlice';
+import customFetch, { checkForUnauthorizedResponse } from '../../utils/axios';
+import { clearValues } from './jobSlice';
+
+export const createJobThunk = async (job, thunkAPI) => {
+
+};
