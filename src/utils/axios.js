8852b0385f3289d5267ca100dd6f307bde43d9ca
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 22:00:53 +0430

    set up

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -14,4 +14,12 @@ customFetch.interceptors.request.use((config) => {
   return config;
 });
 
+export const checkForUnauthorizedResponse = (error, thunkAPI) => {
+  if (error.response.status === 401) {
+    thunkAPI.dispatch(clearStore());
+    return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+  }
+  return thunkAPI.rejectWithValue(error.response.data.msg);
+};
+
 export default customFetch;
