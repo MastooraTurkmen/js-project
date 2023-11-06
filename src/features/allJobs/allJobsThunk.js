15c2e47b03b0643de7875f784f609dede50e0dd2
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:52:30 +0430

    set up

--- src/features/allJobs/allJobsThunk.js
+++ src/features/allJobs/allJobsThunk.js
@@ -1,4 +1,7 @@
 import customFetch, { checkForUnauthorizedResponse } from '../../utils/axios';
 
+export const getAllJobsThunk = async (_, thunkAPI) => {
+  const { page, search, searchStatus, searchType, sort } =
+    thunkAPI.getState().allJobs;
 
 };
