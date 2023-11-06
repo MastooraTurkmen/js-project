62cc2215897fb67abf608438b697777db37a3417
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:53:36 +0430

    set up

---
+++ src/features/job/jobSlice.js
@@ -0,0 +1,21 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import { getUserFromLocalStorage } from '../../utils/localStorage';
+import { createJobThunk, deleteJobThunk, editJobThunk } from './jobThunk';
+const initialState = {
+  isLoading: false,
+  position: '',
+  company: '',
+  jobLocation: '',
+  jobTypeOptions: ['full-time', 'part-time', 'remote', 'internship'],
+  jobType: 'full-time',
+  statusOptions: ['interview', 'declined', 'pending'],
+  status: 'pending',
+  isEditing: false,
+  editJobId: '',
+};
+
+
+export const { handleChange, clearValues, setEditJob } = jobSlice.actions;
+
+export default jobSlice.reducer;
