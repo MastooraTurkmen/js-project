d23207f1a2e5586b7479b10dc1d644a28872d5a2
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:52:38 +0430

    set up

--- src/features/allJobs/allJobsThunk.js
+++ src/features/allJobs/allJobsThunk.js
@@ -4,4 +4,17 @@ export const getAllJobsThunk = async (_, thunkAPI) => {
   const { page, search, searchStatus, searchType, sort } =
     thunkAPI.getState().allJobs;
 
+  let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}`;
+  if (search) {
+    url = url + `&search=${search}`;
+  }
+  try {
+    const resp = await customFetch.get(url);
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
+
+
 };
