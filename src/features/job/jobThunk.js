f9371213c044797957acfc27164d5516d31a9995
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:55:13 +0430

    set up

--- src/features/job/jobThunk.js
+++ src/features/job/jobThunk.js
@@ -11,7 +11,17 @@ export const createJobThunk = async (job, thunkAPI) => {
     return checkForUnauthorizedResponse(error, thunkAPI);
   }
 };
-
+export const deleteJobThunk = async (jobId, thunkAPI) => {
+  thunkAPI.dispatch(showLoading());
+  try {
+    const resp = await customFetch.delete(`/jobs/${jobId}`);
+    thunkAPI.dispatch(getAllJobs());
+    return resp.data.msg;
+  } catch (error) {
+    thunkAPI.dispatch(hideLoading());
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
 export const editJobThunk = async ({ jobId, job }, thunkAPI) => {
   try {
     const resp = await customFetch.patch(`/jobs/${jobId}`, job);
