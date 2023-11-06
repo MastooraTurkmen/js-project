a93f0992bf092d4fa1124723e5bc6ff5d8ef4f72
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-06 21:56:58 +0430

    set up

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -39,6 +39,24 @@ export const updateUser = createAsyncThunk(
   }
 );
 export const clearStore = createAsyncThunk('user/clearStore', clearStoreThunk);
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    toggleSidebar: (state) => {
+      state.isSidebarOpen = !state.isSidebarOpen;
+    },
+    logoutUser: (state, { payload }) => {
+      state.user = null;
+      state.isSidebarOpen = false;
+      removeUserFromLocalStorage();
+      if (payload) {
+        toast.success(payload);
+      }
+    },
+  },
+
+});
 
 export const { toggleSidebar, logoutUser } = userSlice.actions;
 export default userSlice.reducer;
