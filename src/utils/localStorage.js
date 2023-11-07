220fccd11687eb1cb08b9536bc1f28a06a873c08
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-07 22:04:23 +0430

    set up

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,3 +1,7 @@
 export const addUserToLocalStorage = (user) => {
     localStorage.setItem('user', JSON.stringify(user));
 };
+
+export const removeUserFromLocalStorage = () => {
+    localStorage.removeItem('user');
+};
