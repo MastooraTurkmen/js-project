4b61310c55627a6855047eb2bb4b292d1e062934
MastooraTurkmen <janmastoorajan@gmail.com>
2023-11-11 10:08:53 +0430

    styled component Wrapper

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -27,32 +27,36 @@ const Landing = () => {
 }
 
 const Wrapper = styled.main`
-nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-}
-
-.page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -1rem;
-  }
-
-h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
+    nav {
+        width: var(--fluid-width);
+        max-width: var(--max-width);
+        margin: 0 auto;
+        height: var(--nav-height);
+        display: flex;
+        align-items: center;
     }
-  }
 
-p {
-    color: var(--grey-600);
-  }
+    .page {
+        min-height: calc(100vh - var(--nav-height));
+        display: grid;
+        align-items: center;
+        margin-top: -1rem;
+    }
+
+    h1 {
+        font-weight: 700;
+        span {
+        color: var(--primary-500);
+        }
+    }
+
+    p {
+        color: var(--grey-600);
+    }
+
+    .main-img {
+        display: none;
+    }
 
 `;
 
