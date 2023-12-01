f14e6aa0418ca8c763eb59d74134f35a36a644a3
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-01 14:47:03 +0430

    set up react router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
