5f785f96463a789a20994d32afae61c9fe90c3e0
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-01 15:09:01 +0430

    set up react router

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import { Logo } from '../component/index'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
     return (
