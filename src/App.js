8c8df37d5f1edbd271a4545d1e4c9402a4fdf8c0
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-01 14:45:31 +0430

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes,  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Landing, Dashboard, Error, Register } from "./pages";
 
