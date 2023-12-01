028a198b8b30092ce598bc9c23a9dd94fa456a36
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-01 14:45:57 +0430

    set up react router

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import { Landing, Dashboard, Error, Register } from "./pages";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+    </BrowserRouter>
   );
 }
 
