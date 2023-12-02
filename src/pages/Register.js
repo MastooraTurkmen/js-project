06d2197f84389deff5753362d5e7399df9ff11d4
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-02 16:34:30 +0430

    set up form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -28,7 +28,9 @@ const Register = () => {
                 <h3>Login</h3>
                 {/* name field */}
                 <div className="form-row">
-                    <label htmlFor="name" className="form-label"></label>
+                    <label htmlFor="name" className="form-label">
+                        name
+                    </label>
                 </div>
             </form>
         </Wrapper>
