e541caf95991c09421a77b73d4db6efabb35f7e3
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-02 16:21:17 +0430

    set up

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,10 +2,14 @@ import { useState, useEffect } from "react";
 import { Logo } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 
+const initialState = {
+
+}
+
 const Register = () => {
     return (
         <Wrapper>
-            
+
         </Wrapper>
     )
 }
