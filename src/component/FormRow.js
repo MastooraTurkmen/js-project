d4ade17de1eaf8d4cbc36d00de21d3d4915f3dd1
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-03 17:33:25 +0430

    formRow

--- src/component/FormRow.js
+++ src/component/FormRow.js
@@ -1,4 +1,4 @@
-const FormRow = () => {
+const FormRow = ({ type, name, value, handleChange, labelText }) => {
     return (
         <div className="form-row">
             <label htmlFor="name" className="form-label">
