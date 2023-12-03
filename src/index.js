732a6912ef5cece5fe5c2a73ae2a7c3de7e6fb19
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-03 17:01:45 +0430

    set up react

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tab="home" />
   </React.StrictMode>
 );
