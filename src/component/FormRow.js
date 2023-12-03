eb2aed69add91ea2482384338b1eecbe453e7dd9
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-03 17:34:35 +0430

    formRow

--- src/component/FormRow.js
+++ src/component/FormRow.js
@@ -1,15 +1,15 @@
 const FormRow = ({ type, name, value, handleChange, labelText }) => {
     return (
         <div className="form-row">
-            <label htmlFor="name" className="form-label">
-                name
+            <label htmlFor={name} className="form-label">
+                {name}
             </label>
             <input
                 className="form-input"
-                type="text"
-                name="name"
+                type={type}
+                name={name}
                 onChange={handleChange}
-                value={values.name}
+                value={value}
             />
         </div>
     )
