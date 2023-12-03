f276f12b2751a053b3bd692a16f532178ae912b4
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-03 17:36:04 +0430

    formRow

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -27,7 +27,7 @@ const Register = () => {
                 <Logo />
                 <h3>Login</h3>
                 {/* name field */}
-                <FormRow type='text' name='name' />
+                <FormRow type='text' name='name' value={values.name} handleChange={handleChange} />
                 <button type="button" className="btn btn-block">submit</button>
             </form>
         </Wrapper>
