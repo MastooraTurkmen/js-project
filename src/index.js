8de95c09dbc335520139fcddceef675376010e4a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-08 19:29:32 +0430

    store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'normalize.css'
 import './index.css';
 import App from './App';
+import { store } from './pages/store';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App tab="home" />);
