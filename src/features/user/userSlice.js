94ab867de29db462e0d04f4aa736867965044e4d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-08 19:26:55 +0430

    userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -3,4 +3,12 @@ import { toast } from "react-toastify";
 
 const initialState = {
     isLoading: false,
+    user: null,
 }
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+})
+
+export default userSlice.reducer;
