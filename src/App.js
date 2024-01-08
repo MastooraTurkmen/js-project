a976c9f87955f131f8b6a9d32cdcf15647769228
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-08 18:37:05 +0430

    toastify

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <ToastContainer />
     </BrowserRouter>
   );
 }
