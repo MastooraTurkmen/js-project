8a9a8d359797dcb18dd8a0bc01cd225c9f2cc20d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-09 16:51:18 +0430

    localStorage

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -2,3 +2,7 @@
 export const addUserToLocalStorage = (user) => {
     localStorage.setItem('user', JSON.stringify(user));
 }
+
+export const removeUserFromLocalStorage = (user) => {
+    localStorage.removeItem('user');
+}
