5156df78f91bcf57e010f3c6d8669135d835329e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-09 11:09:52 +0430

    create slice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -23,21 +23,23 @@ export const loginUser = createAsyncThunk('user/loginUser', async (user, thunkAP
 const userSlice = createSlice({
     name: 'user',
     initialState,
-    extraReducers: {
-        [registerUser.pending]: (state) => {
-            state.isLoading = true;
-        },
-        [registerUser.fulfilled]: (state, { payload }) => {
-            const { user } = payload;
-            state.isLoading = false;
-            state.user = user;
-            toast.success(`Hello There ${user.name}`)
-        },
-        [registerUser.rejected]: (state, { payload }) => {
-            state.isLoading = false;
-            toast.error(payload)
-        },
-    }
-})
+    extraReducers: (builder) => {
+        builder
+            .addCase(registerUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(registerUser.fulfilled, (state, { payload }) => {
+                const { user } = payload;
+                state.isLoading = false;
+                state.user = user;
+                toast.success(`Hello There ${user.name}`);
+            })
+            .addCase(registerUser.rejected, (state, { payload }) => {
+                state.isLoading = false;
+                toast.error(payload);
+            });
+    },
+});
+
 
 export default userSlice.reducer;
