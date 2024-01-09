133903f89f4bb0149b2da16a38655b7d3d004582
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-09 10:37:10 +0430

    set up

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -69,7 +69,7 @@ const Register = () => {
                     value={values.password}
                     handleChange={handleChange}
                 />
-                <button type="submit" className="btn btn-block">submit</button>
+                <button type="submit" className="btn btn-block" disabled={isLoading}>submit</button>
                 <p> {values.isMember ? 'Not a member yet.' : 'Already a member?'}
                     <button type="button" className="member-btn" onClick={toggleMemeber}>
                         {values.isMember ? 'Register' : 'Login'}
