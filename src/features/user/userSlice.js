4450b1a1e3642ef52f4b6b0c767a7d0aec45a896
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-09 08:43:12 +0430

    set up axios

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-
+import customFetch from "./utils/axios";
 
 const initialState = {
     isLoading: false,
