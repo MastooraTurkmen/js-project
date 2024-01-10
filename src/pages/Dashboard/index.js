8e252bb3d481f2e858af9e025e9519b8205c1cb1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-10 17:42:00 +0430

    set up

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,7 @@
 import AddJob from "./AddJob";
 import AllJobs from "./AllJobs";
 import Profile from "./Profile";
-import States from './States'
+import States from './States';
+import SharedLayout from './SharedLayout';
 
-export default { Profile, AddJob, AllJobs, States };
+export default { Profile, AddJob, AllJobs, States, SharedLayout };
