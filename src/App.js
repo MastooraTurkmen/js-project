6ffef7b4c4ab9b8c9bee4cc53a13c09ad914276e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-10 17:44:17 +0430

    set up

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing, Error, Register } from "./pages";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Profile, AddJob, AllJobs, States, SharedLayout } from './pages/index'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ } />
+        <Route path="/" element={<SharedLayout />} />
         <Route path="landing" element={<Landing />} />
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
