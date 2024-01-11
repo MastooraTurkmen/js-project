142fbf2f1249b556784ae33fee1190f2cca283fa
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-11 17:47:44 +0430

    dashboard

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Landing, Error, Register } from "./pages";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Profile, AddJob, AllJobs, States, SharedLayout } from './pages/index'
+import { Profile, AddJob, AllJobs, States, SharedLayout } from './pages/Dashboard'
 
 function App() {
   return (
