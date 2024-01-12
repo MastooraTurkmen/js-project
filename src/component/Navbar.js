262ec92a3ff3be104461c9555e8839aa8fbdf319
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-12 18:22:05 +0430

    react icon

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,5 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome, FaUserCircle } from 'react-icons/fa';
+import { FaHome, FaUserCircle, FaCaretDown } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
