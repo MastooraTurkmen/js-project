023660d46a7e7118711a1f93374e0062a1a3cb93
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-12 18:24:18 +0430

    set up

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,10 +2,11 @@ import Wrapper from "../assets/wrappers/Navbar";
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
 import Logo from "./Logo";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
     const { user } = useSelector((store) => store.user)
+    const dispatch = useDispatch()
 
     return (
         <Wrapper>
