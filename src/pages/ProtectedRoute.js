57fd0f3c420bfac7628de09241d9a83ffe2320ea
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-12 19:36:47 +0430

    set up

--- src/pages/ProtectedRoute.js
+++ src/pages/ProtectedRoute.js
@@ -8,9 +8,7 @@ const ProtectedRoute = ({ children }) => {
         return <Navigate to='/landing' />
     }
 
-    return (
-        <div>ProtectedRoute</div>
-    )
+    return children;
 }
 
 export default ProtectedRoute
