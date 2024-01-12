b7ecca40c83c0257eea72a7cf8e9c9e135c594ad
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-12 19:37:15 +0430

    set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Landing from "./Landing";
 import Error from "./Error";
 import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
 
-export { Landing, Error, Register }
+
+export { Landing, Error, Register, ProtectedRoute }
