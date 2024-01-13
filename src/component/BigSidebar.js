7f3a2a051408acaae37a2ffd4844d8ebe31cf265
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-13 20:12:22 +0430

    set up

--- src/component/BigSidebar.js
+++ src/component/BigSidebar.js
@@ -4,6 +4,8 @@ import Logo from "../components/Logo";
 import { useSelector } from "react-redux";
 
 const BigSidebar = () => {
+    const { isSidebarOpen } = useSelector()
+
     return (
         <Wrapper>
             <div>BigSidebar</div>
