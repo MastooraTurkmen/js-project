ed706585fcb12a9cd33bc584fdcae985abc878e3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-13 19:44:21 +0430

    set up

--- src/component/SmallSidebar.js
+++ src/component/SmallSidebar.js
@@ -1,7 +1,7 @@
 import Wrapper from "../assets/wrappers/SmallSidebar"
 import { FaTimes } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-
+import Logo from "./Logo";
 
 const SmallSidebar = () => {
     return (
