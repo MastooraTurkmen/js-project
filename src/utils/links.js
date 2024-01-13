dc07f383ac11390c5f228551cf8fb7132162636d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-13 19:55:38 +0430

    set up

--- src/utils/links.js
+++ src/utils/links.js
@@ -2,3 +2,7 @@ import { IoBarChartSharp } from "react-icons/io5";
 import { MdQueryStats } from "react-icons/md";
 import { FaWpforms } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
+
+const links = []
+
+export default links;
