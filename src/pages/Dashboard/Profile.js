43a7b0bf92efa6d353cb6bf568e8d6113c2fd618
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-14 16:55:28 +0430

    set up profile

--- src/pages/Dashboard/Profile.js
+++ src/pages/Dashboard/Profile.js
@@ -11,6 +11,7 @@ const Profile = () => {
         name: user?.name || '',
         email: user?.email || '',
         lastName: user?.lastName || '',
+        location: user?.location || '',
     })
 
     return (
