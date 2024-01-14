dfc45f186fe821f7d496c4da62820ef1d868fa11
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-14 20:26:11 +0430

    set up

--- src/component/NavLinks.js
+++ src/component/NavLinks.js
@@ -4,7 +4,23 @@ import links from "../utils/links";
 const NavLinks = ({ toggleSidebar }) => {
     return (
         <div className="nav-links">
-            
+            {links.map((link) => {
+                const { text, path, id, icon } = link;
+
+                return (
+                    <NavLink
+                        to={path}
+                        key={id}
+                        onClick={toggleSidebar}
+                        className={({ isActive }) =>
+                            isActive ? "nav-link active" : "nav-link"
+                        }
+                    >
+                        <span className="icon">{icon}</span>
+                        {text}
+                    </NavLink>
+                );
+            })}
         </div>
     );
 };
