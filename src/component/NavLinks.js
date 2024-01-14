49fec47afd8bc1a0d26496f30468b3b9759653d5
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-14 20:25:51 +0430

    set up

--- src/component/NavLinks.js
+++ src/component/NavLinks.js
@@ -2,7 +2,11 @@ import { NavLink } from "react-router-dom";
 import links from "../utils/links";
 
 const NavLinks = ({ toggleSidebar }) => {
-    
+    return (
+        <div className="nav-links">
+            
+        </div>
+    );
 };
 
 export default NavLinks;
