061222e509634e121c9f48bd780b1ce315194130
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-15 19:09:24 +0430

    job

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../features/user/userSlice";
+import jobSlice from "../features/job/jobSlice";
 
 export const store = configureStore({
     reducer: {
-        user: userSlice
+        user: userSlice,
+        job: jobSlice
     }
 })
