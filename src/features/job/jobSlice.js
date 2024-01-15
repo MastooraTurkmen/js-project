60c22597d6be2d0f7c7f747aed3f9161437397f5
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-15 19:06:14 +0430

    job

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -3,3 +3,15 @@ import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
 import { getUserFromLocalStorage } from "../../utils/localStorage";
 
+const initialState = {
+    isLoading: false,
+    position: "",
+    company: "",
+    jobLocation: "",
+    jobTypeOptions: ["full-time", "part-time", "remote", "internship"],
+    jobType: "full-time",
+    statusOptions: ["interview", "declined", "pending"],
+    status: "pending",
+    isEditing: false,
+    editJobId: "",
+};
