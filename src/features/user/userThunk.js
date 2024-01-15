7584b1b6b9842495700b79888abaf948eaac9ddd
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-15 18:05:14 +0430

    user thunk

--- src/features/user/userThunk.js
+++ src/features/user/userThunk.js
@@ -2,5 +2,10 @@ import customFetch from "../../utils/axios";
 import { logoutUser } from "./userSlice";
 
 export const registerUserThunk = async (url, user, thunkAPI) => {
-    
+    try {
+        const resp = await customFetch.post('auth/register', user)
+        return resp.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
 }
