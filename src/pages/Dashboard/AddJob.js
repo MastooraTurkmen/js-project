17d712d7f27ad673244efda45eb02b2905d0b6e3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-15 09:14:35 +0430

    job

--- src/pages/Dashboard/AddJob.js
+++ src/pages/Dashboard/AddJob.js
@@ -1,6 +1,7 @@
 import { FormRow } from "../../components";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
 const AddJob = () => {
     return (
