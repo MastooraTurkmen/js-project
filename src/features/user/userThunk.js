421d9eb2227bc973833c03d343bdc0415b0be0e8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-15 18:09:07 +0430

    user thunk

--- src/features/user/userThunk.js
+++ src/features/user/userThunk.js
@@ -20,5 +20,18 @@ export const loginUserThunk = async (url, user, thunkAPI) => {
 }
 
 export const updateUserThunk = async (url, user, thunkAPI) => {
-
+    try {
+        const resp = await customFetch.post('auth/updateUser', user, {
+            headers: {
+                authorization: `Bearer ${thunkAPI.getState().user.user.token}`
+            }
+        })
+        return resp.data;
+    } catch (error) {
+        if (error.response.status === 401) {
+            thunkAPI.dispatch(logoutUser());
+            return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+        }
+        return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
 }
