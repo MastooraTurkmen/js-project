db96eb2953584a4f1a09031aa77612ac83d17d53
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-15 18:05:24 +0430

    user thunk

--- src/features/user/userThunk.js
+++ src/features/user/userThunk.js
@@ -3,7 +3,7 @@ import { logoutUser } from "./userSlice";
 
 export const registerUserThunk = async (url, user, thunkAPI) => {
     try {
-        const resp = await customFetch.post('auth/register', user)
+        const resp = await customFetch.post(url, user)
         return resp.data;
     } catch (error) {
         return thunkAPI.rejectWithValue(error.response.data.msg)
