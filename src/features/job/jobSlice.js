b6a7d72ca8cbf665d36eb17b27f633914d5b8d5a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-15 19:06:06 +0430

    job

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,3 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
+import { getUserFromLocalStorage } from "../../utils/localStorage";
+
