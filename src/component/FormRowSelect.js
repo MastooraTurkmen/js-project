842a260aa00ff810bbb6224a77e2bc7218ca8acb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-16 10:02:17 +0430

    form row select

--- src/component/FormRowSelect.js
+++ src/component/FormRowSelect.js
@@ -1,8 +1,13 @@
 
 const FormRowSelect = () => {
-  return (
-    <div>FormRowSelect</div>
-  )
+    return (
+        <div className="form-row">
+            <label htmlFor={name} className="form-label">
+                {labelText || name}
+            </label>
+           
+        </div>
+    );
 }
 
 export default FormRowSelect
