a4bbb4e8b4babe2bbf5bf901fd4845fc5d32e4e0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-16 09:20:52 +0430

    job

--- src/pages/Dashboard/AddJob.js
+++ src/pages/Dashboard/AddJob.js
@@ -20,6 +20,7 @@ const AddJob = () => {
     const handleJobInput = (e) => {
         const name = e.target.name;
         const value = e.target.value;
+        console.log(name, value);
     }
 
     return (
