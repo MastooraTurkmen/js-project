2ddf5bc8c739807a3dc51d776572dbf4ea7b4ce5
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-16 09:59:04 +0430

    form row select

--- src/component/index.js
+++ src/component/index.js
@@ -3,5 +3,6 @@ import FormRow from "./FormRow";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
+import FormRowSelect from "./FormRowSelect";
 
-export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar }
+export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar, FormRowSelect }
