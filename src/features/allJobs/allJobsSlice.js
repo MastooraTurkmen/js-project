ab9e14e662a06ed5e0fd677c404ec45b040119bd
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-17 17:11:44 +0430

    set up all jobs

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -1,3 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
+
+const initialFiltersState = {
+    search: "",
+    searchStatus: "all",
+    searchType: "all",
+    sort: "latest",
+    sortOptions: ["latest", "oldest", "a-z", "z-a"],
+};
+
