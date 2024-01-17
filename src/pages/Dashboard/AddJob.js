57a9968157b7e23ed66c3bc2d03c62da14ece5c3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-17 10:41:54 +0430

    handle change

--- src/pages/Dashboard/AddJob.js
+++ src/pages/Dashboard/AddJob.js
@@ -23,7 +23,7 @@ const AddJob = () => {
     const handleJobInput = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        console.log(name, value);
+        dispatch(handleChange({ name, value }))
     }
 
     return (
