ff2f5417b72844f9b9b82b656b587afd3c1ca773
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-18 18:08:56 +0430

    set up all jobs page

--- src/component/JobsContainer.js
+++ src/component/JobsContainer.js
@@ -6,7 +6,13 @@ import { useSelector, useDispatch } from "react-redux";
 const JobsContainer = () => {
     const { jobs, isLoading } = useSelector((store) => store.allJobs);
     const dispatch = useDispatch();
-    
+
+    if (isLoading) {
+        return <Wrapper>
+            <h2>Loading...</h2>
+        </Wrapper>
+    }
+
     return (
         <h1>JobsContainer</h1>
     )
