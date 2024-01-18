a3a482c39fd40391dc76de2266d323288149233f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-18 18:07:54 +0430

    set up all jobs page

--- src/component/JobsContainer.js
+++ src/component/JobsContainer.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
+import { useSelector, useDispatch } from "react-redux";
 
 const JobsContainer = () => {
     return (
