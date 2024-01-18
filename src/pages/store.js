38a9e37cfdde71dcd2d37179e67e1e7460be21c6
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-18 17:15:29 +0430

    set up all jobs

--- src/pages/store.js
+++ src/pages/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../features/user/userSlice";
 import jobSlice from "../features/job/jobSlice";
-import allJobsSlice from "../features/job/allJobsSlice";
+import allJobsSlice from "../features/allJobs/allJobsSlice";
 
 export const store = configureStore({
     reducer: {
