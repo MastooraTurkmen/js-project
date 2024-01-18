a9f00aa8ba358bf98249f5008f77c9ba53110fe5
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-18 17:58:53 +0430

    set up all jobs page

--- src/component/index.js
+++ src/component/index.js
@@ -4,5 +4,7 @@ import Logo from "./Logo";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
 import FormRowSelect from "./FormRowSelect";
+import Job from "./Job";
 
-export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar, FormRowSelect }
+
+export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar, FormRowSelect, Job }
