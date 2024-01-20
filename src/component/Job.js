1fc0f6d899bd93523efb4573dbb398d5d062d6c1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-20 07:50:44 +0430

    set up

--- src/component/Job.js
+++ src/component/Job.js
@@ -1,6 +1,7 @@
 import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Job";
+import { useDispatch } from "react-redux";
 
 const Job = () => {
     return (
