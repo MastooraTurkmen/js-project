3ab438bf7d3d5f8e17588e79b1daf10af79c5fcb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-20 08:36:09 +0430

    loading

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
 import { getUserFromLocalStorage } from "../../utils/localStorage";
-import { showLoading } from "../allJobs/allJobsSlice";
+import { showLoading, hideLoading, getAllJobs } from "../allJobs/allJobsSlice";
 
 const initialState = {
     isLoading: false,
