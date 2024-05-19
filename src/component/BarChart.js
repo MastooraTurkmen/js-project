9d0f0e0f84c8feefc267d46ffaa05964f0f02f48
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-19 17:46:59 +0430

    bar chart

--- src/component/BarChart.js
+++ src/component/BarChart.js
@@ -12,11 +12,11 @@ const BarChartComponent = ({ data }) => {
     return (
         <ResponsiveContainer width="100%" height={300}>
             <BarChart data={data} margin={{ top: 50 }}>
-                <CartesianGrid strokeDasharray="3 3 " />
+                <CartesianGrid strokeDasharray="10 10 " />
                 <XAxis dataKey="date" />
                 <YAxis allowDecimals={false} />
                 <Tooltip />
-                <Bar dataKey="count" fill="#3b82f6" barSize={75} />
+                <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
             </BarChart>
         </ResponsiveContainer>
     );
