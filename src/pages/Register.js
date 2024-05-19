ead05172349f1c7fe47c2b1ea4cbb47533a03c00
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-19 14:29:58 +0430

    test user

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -82,6 +82,11 @@ const Register = () => {
                 <button type="submit" className="btn btn-block" disabled={isLoading}>
                     {isLoading ? 'loading...' : 'submit'}
                 </button>
+                <button type="button" className="btn btn-block btn-hipster"
+                    onClick={() => dispatch(loginUser({ email: 'testUser@test.com', password: 'secret' }))}
+                    disabled={isLoading}>
+                    {isLoading ? 'loading...' : 'demo app'}
+                </button>
                 <p> {values.isMember ? 'Not a member yet.' : 'Already a member?'}
                     <button type="button" className="member-btn" onClick={toggleMemeber}>
                         {values.isMember ? 'Register' : 'Login'}
