d521089be6246eea6c085836d565a7c3d12c5bc9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-19 14:21:32 +0430

    auth header

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,17 @@
 import axios from "axios"
+import { getUserFromLocalStorage } from "./localStorage"
 
 const customFetch = axios.create({
-    baseURL: 'https://redux-toolkit-jobster-api-server.onrender.com/api/v1',
+    baseURL: 'https://jobify-prod.herokuapp.com/api/vi/toolkit',
+    // baseURL: 'https://redux-toolkit-jobster-api-server.onrender.com/api/v1',
+})
+
+customFetch.interceptors.request.use((confiq) => {
+    const user = getUserFromLocalStorage()
+    if (user) {
+        confiq.headers.common['Authorization'] = `Bearer ${user.token}`;
+    }
+    return confiq
 })
 
 export default customFetch;
