23a459dc1b3792d2b968f91d37d9d37284e4475a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-19 18:23:31 +0430

    search container

--- src/component/SearchContainer.js
+++ src/component/SearchContainer.js
@@ -1,3 +1,7 @@
+import { FormRow, FormRowSelect } from ".";
+import Wrapper from "../assets/wrappers/SearchContainer";
+import { useSelector, useDispatch } from "react-redux";
+
 const SearchContainer = () => {
     return (
         <h2>SearchContainer</h2>
