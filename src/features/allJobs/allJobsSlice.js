5b7a2869abcd1816c7b0dcafa64a96350dcf534f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-19 16:52:15 +0430

    stats page

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -35,6 +35,16 @@ export const getAllJobs = createAsyncThunk(
     }
 );
 
+export const showStats = createAsyncThunk('allJobs/showStats', async (_, thunkAPI) => {
+    try {
+        const resp = await customFetch.get('/jobs/stats')
+        console.log(resp.data);
+        return resp.data
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
+})
+
 const allJobsSlice = createSlice({
     name: 'allJobs',
     initialState,
@@ -56,6 +66,18 @@ const allJobsSlice = createSlice({
                 state.jobs = payload.jobs
             })
             .addCase(getAllJobs.rejected, (state, { payload }) => {
+                state.isLoading = false;
+                toast.error(payload)
+            })
+            .addCase(showStats.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(showStats.fulfilled, (state, { payload }) => {
+                state.isLoading = false;
+                state.stats = payload.defaultStats
+                state.monthlyApplications = payload.monthlyApplications
+            })
+            .addCase(showStats.rejected, (state, { payload }) => {
                 state.isLoading = false;
                 toast.error(payload)
             })
