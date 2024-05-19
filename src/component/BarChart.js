5596024038add6edd637e98c68caf20aa4fd9874
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-19 17:47:31 +0430

    bar chart

--- src/component/BarChart.js
+++ src/component/BarChart.js
@@ -16,7 +16,7 @@ const BarChartComponent = ({ data }) => {
                 <XAxis dataKey="date" />
                 <YAxis allowDecimals={false} />
                 <Tooltip />
-                <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+                <Bar dataKey="count" fill="#3b82f6" barSize={75} />
             </BarChart>
         </ResponsiveContainer>
     );
