5a36104ce6fe4762fd3e0f9417e6711a5cfbdfc9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-19 18:23:54 +0430

    search container

--- src/component/SearchContainer.js
+++ src/component/SearchContainer.js
@@ -3,6 +3,15 @@ import Wrapper from "../assets/wrappers/SearchContainer";
 import { useSelector, useDispatch } from "react-redux";
 
 const SearchContainer = () => {
+    const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+        useSelector((store) => store.allJobs);
+    const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+    const dispatch = useDispatch();
+    const handleSearch = (e) => { };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <h2>SearchContainer</h2>
     )
