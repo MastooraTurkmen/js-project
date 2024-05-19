044748c18906539aceede95b65184775f51548ac
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-19 16:52:16 +0430

    stats page

--- src/pages/Dashboard/States.js
+++ src/pages/Dashboard/States.js
@@ -1,6 +1,23 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { showStats } from "../../features/allJobs/allJobsSlice"
+import { StatsContainer, Loading, ChartsContainer } from "../../component"
+
 const States = () => {
+    const { isLoading, monthlyApplications } = useSelector((store) => store.allJobs)
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(showStats())
+    }, [])
+
+
     return (
-        <div>States</div>
+        <>
+            <StatsContainer />
+            {monthlyApplications.length > 0 && <ChartsContainer />}
+        </>
+
     )
 }
 
