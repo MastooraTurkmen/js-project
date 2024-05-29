5013d51717a289e0bcd94ca60ef3a61d773aed06
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-29 17:11:58 +0430

    pagination

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -70,7 +70,9 @@ const allJobsSlice = createSlice({
             })
             .addCase(getAllJobs.fulfilled, (state, { payload }) => {
                 state.isLoading = false;
-                state.jobs = payload.jobs
+                state.jobs = payload.jobs;
+                state.numOfPages = payload.numOfPages;
+                state.totalJobs = payload.totalJobs;
             })
             .addCase(getAllJobs.rejected, (state, { payload }) => {
                 state.isLoading = false;
