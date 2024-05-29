b74e93cf85ce6902c59efe71c14b950c7f91c3eb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-29 17:11:52 +0430

    pagination

--- src/component/JobsContainer.js
+++ src/component/JobsContainer.js
@@ -4,9 +4,10 @@ import Wrapper from "../assets/wrappers/JobsContainer";
 import { useSelector, useDispatch } from "react-redux";
 import Loading from "./Loading";
 import { getAllJobs } from "../features/allJobs/allJobsSlice";
+import PageBtnContainer from "./PageBtnContainer";
 
 const JobsContainer = () => {
-    const { jobs, isLoading } = useSelector((store) => store.allJobs);
+    const { jobs, isLoading, page, totalJobs, numOfPages } = useSelector((store) => store.allJobs);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -27,13 +28,14 @@ const JobsContainer = () => {
 
     return (
         <Wrapper>
-            <h5>jobs info</h5>
+            <h5>{totalJobs} job{jobs.length > 1 && 's'} Found</h5>
             <div className="jobs">
                 {jobs.map((job) => {
                     console.log(job);
                     return <Job key={job._id} {...job} />
                 })}
             </div>
+            {numOfPages > 1 && <PageBtnContainer />}
         </Wrapper>
     )
 }
