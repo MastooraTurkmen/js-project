e42f97a9764d14c77cb6866625460297df8bf3c9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-29 19:52:25 +0430

    query params

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -24,8 +24,12 @@ const initialState = {
 export const getAllJobs = createAsyncThunk(
     'allJobs/getJobs',
     async (_, thunkAPI) => {
-        let url = `/jobs`;
+        const { search, page, searchStatus, searchType, sort } = thunkAPI.getState().allJobs
+        let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}}&search=${search}`;
 
+        if (search) {
+            url = url + `&search=${search}`
+        }
         try {
             const resp = await customFetch.get(url);
             return resp.data;
