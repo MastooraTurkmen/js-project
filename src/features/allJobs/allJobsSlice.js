ce26aea413bd3639500351c08f8826b1123edf6e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-29 19:03:59 +0430

    pagination

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -61,6 +61,9 @@ const allJobsSlice = createSlice({
         },
         clearFilters: (state) => {
             return { ...state, ...initialFiltersState }
+        },
+        changePage: (state, { payload }) => {
+            state.page = payload;
         }
     },
     extraReducers: (builder) => {
@@ -93,5 +96,5 @@ const allJobsSlice = createSlice({
     }
 });
 
-export const { showLoading, hideLoading, handleChange, clearFilters } = allJobsSlice.actions;
+export const { showLoading, hideLoading, handleChange, clearFilters, changePage } = allJobsSlice.actions;
 export default allJobsSlice.reducer;
